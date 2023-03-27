f471ce3b1856b880ab02178579c9f1fffa93a269
cowboy <39163647+leeo2020@users.noreply.github.com>
2023-03-27 08:05:54 +0800

    Add files via upload

--- ngnx.c
+++ ngnx.c
@@ -1,32 +1,74 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <Windows.h>
+
+char nginxDir[1024] = {0};
+
+const char *findNginx();
+void run(int argc, char *args[], const char *nginxUrl);
 
 int main(int argc, char *args[])
 {
+	const char *nginxUrl = findNginx();
+	run(argc, args, nginxUrl);
+}
+
+const char *findNginx()
+{
+	char buffer[1024] = {0};
+	const char *command = "where nginx";
+	FILE *fp = _popen(command, "r");
+	fgets(buffer, sizeof(buffer), fp);
+	_pclose(fp);
+	strncpy(nginxDir, buffer, strlen(buffer) - strlen("\\nginx.exe"));
+	return nginxDir;
+}
+
+void run(int argc, char *args[], const char *nginxUrl)
+{
+	char cmd[] = {"cd /d "};
+	strcat(cmd, nginxUrl);
 	if (argc < 2)
 	{
-		system("cd /d E:\\PHP\\PhpDev\\nginx-1.19.2 && start nginx");
+		strcat(cmd, " && start nginx");
+		system(cmd);
 	}
 	else
 	{
 		if (strcmp(args[1], "run") == 0 || strcmp(args[1], "start") == 0)
 		{
-			system("cd /d E:\\PHP\\PhpDev\\nginx-1.19.2 && start nginx");
+			strcat(cmd, " && start nginx");
+			system(cmd);
 		}
 		else if (strcmp(args[1], "reload") == 0 || strcmp(args[1], "restart") == 0)
 		{
-			system("cd /d E:\\PHP\\PhpDev\\nginx-1.19.2 && nginx -s reload");
+			strcat(cmd, " && nginx -s reload");
+			system(cmd);
 		}
 		else if (strcmp(args[1], "exit") == 0 || strcmp(args[1], "quit") == 0 || strcmp(args[1], "shtd") == 0 || strcmp(args[1], "stop") == 0)
 		{
-			system("cd /d E:\\PHP\\PhpDev\\nginx-1.19.2 && nginx -s quit");
+			strcat(cmd, " && nginx -s quit");
+			system(cmd);
+		}
+		else if (strcmp(args[1], "shtd") == 0 || strcmp(args[1], "stop") == 0)
+		{
+			system("taskkill /f /t /im nginx.exe");
+		}
+		else if (strcmp(args[1], "-h") == 0 || strcmp(args[1], "-help") == 0)
+		{
+			printf("to start nginx, please run ngnx, ngnx run or ngnx start \n");
+			Sleep(25);
+			printf("to restart nginx, please run ngnx reload or ngnx restart \n");
+			Sleep(25);
+			printf("to quit nginx, ngnx exit or ngnx quit \n");
+			Sleep(25);
+			printf("to force shutdown nginx, please run ngnx stop, ngnx shtd \n");
+			Sleep(25);
 		}
 		else
 		{
 			;
 		}
 	}
-
-	return 0;
 }
